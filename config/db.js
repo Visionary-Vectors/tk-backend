74106b0d9705911d6d2dcd65d456af251b115e86
codebyjsa <jsaishere@gmail.com>
2025-07-26 14:05:27 +0530

    render deployment

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.SUPABASE_DB_URL, // Use env variable for security
+  connectionString: process.env.DATABASE_URL, // Use env variable for security
   ssl: {
     rejectUnauthorized: false, // Required for Supabase SSL
   },
